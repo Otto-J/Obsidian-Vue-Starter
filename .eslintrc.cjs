dbe024b428d4a40e66c0c534d08c15cae2d39672
Otto <fa@ijust.cc>
2023-10-23 14:53:32 +0800

    style: 格式化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,17 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-    root: true,
-    extends: [
-        "plugin:vue/vue3-essential",
-        "eslint:recommended",
-        "@vue/eslint-config-typescript",
-        "@vue/eslint-config-prettier/skip-formatting",
-    ],
-    parserOptions: {
-        ecmaVersion: "latest",
-    },
-    rules: {
-        "vue/multi-word-component-names": "off",
-    },
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
 };
